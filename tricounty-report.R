e8f4c41a504852710836c126e9dbce96a9427ee8
Andrew <pulsipher.a@gmail.com>
2025-06-02 12:45:44 -0600

    Add graceful failure to puglic_disease_list access

--- tricounty-report.R
+++ tricounty-report.R
@@ -34,6 +34,53 @@ write_public_report <- function(data, filename) {
 # ------------------------------------------------------------------------------
 # Define Helper Functions ------------------------------------------------------
 
+# Function to validate data
+validate_data <- function(data) {
+  # Check column names
+  expected_colnames <- c("patient_mmwr_year", "patient_mmwr_week", "patient_disease")
+  actual_colnames <- colnames(data)
+  
+  if (!all(expected_colnames %in% actual_colnames)) {
+    stop("The EpiTrax data is missing one of the following fields:\n\n\t",
+         paste(expected_colnames, collapse=", "), 
+         "\n\nPlease add the missing fields to the file and try again.")
+  }
+  # Check column data types
+  if (class(data$patient_mmwr_week) != "integer" ||
+      class(data$patient_mmwr_year) != "integer" ||
+      class(data$patient_disease) != "character") {
+    stop("One or more columns in the EpiTrax dataset has an incorrect 
+         data type:\n\n",
+         "\t'patient_mmwr_week' should be of type 'integer'\n",
+         "\t'patient_mmwr_year' should be of type 'integer'\n",
+         "\t'patient_disease' should be of type 'character'\n",
+         "\nPlease try again with a valid dataset."
+    )
+  }
+  # Remove all columns we're not using
+  # - Note this also rearranges the columns into the order we expect below
+  data <- data[expected_colnames]
+  
+  data
+}
+
+# Function to format EpiTrax data using 'patient_mmwr_week' column
+format_week_num <- function(data) {
+  # Validate data
+  data <- validate_data(data)
+  
+  # Format data
+  data$month <- with(data, month(
+    ymd(patient_mmwr_year * 10000 + 0101) + 
+      patient_mmwr_week * 7
+  ))
+  data$patient_mmwr_week <- NULL
+  data$counts <- 1 # Makes easier to use aggregate()
+  colnames(data) <- c("year", "disease", "month", "counts")
+
+  data
+}
+
 # Function to read in input EpiTrax file
 read_epitrax_data <- function() {
   # Get file name from input data folder
@@ -54,6 +101,9 @@ read_epitrax_data <- function() {
   # Read data from file
   input_data <- read.csv(fpath, header = TRUE)
   
+  # Validate and format data
+  input_data <- format_week_num(input_data)
+  
   # Move processed input file to processed_data_folder
   # - Comment to disable this behavior
   # file.rename(fpath, file.path(processed_data_folder, fname))
@@ -62,39 +112,40 @@ read_epitrax_data <- function() {
   input_data
 }
 
-format_onset_date <- function(data) {
-  data$year <- as.integer(
-    format(as.Date(data$patient_disease_onset_date,"%m/%d/%Y"), "%Y")
-  )
-  data$month <- as.integer(
-    format(as.Date(data$patient_disease_onset_date,"%m/%d/%Y"), "%m")
-  )
-  data$patient_disease_onset_date <- NULL
-  data$patient_mmwr_year <- NULL
-  colnames(data) <- c("disease", "year", "month")
-  data
-}
-
-format_week_num <- function(data) {
-  data$month <- with(data, month(
-    ymd(patient_mmwr_year * 10000 + 0101) + 
-      patient_mmwr_week * 7
-  ))
-  data$patient_mmwr_week <- NULL
-  data$counts <- 1 # Makes easier to use aggregate()
-  colnames(data) <- c("year", "disease", "month", "counts")
-  data
+# Function to read in the public disease list (CSV)
+get_public_disease_list <- function(epitrax_data) {
+  fname <- "disease_list_for_public_report.csv"
+  fpath <- file.path(public_reports_folder, fname)
+  
+  # If the file doesn't exist, the function will return the list of diseases
+  # - from the input EpiTrax dataset instead and give the user a warning
+  if (!file.exists(fpath) || !grepl("\\.csv$", fpath)) {
+    warning("File '", fname, "' not found in '", public_reports_folder,
+            "' folder. Using list from EpiTrax input dataset instead.")
+    
+    diseases <- sort(unique(epitrax_data$disease))
+    disease_list <- data.frame(
+      EpiTrax_name = diseases,
+      Public_name = diseases
+    )
+    
+    disease_list
+  } else {
+    disease_list <- read.csv(fpath, header = TRUE)
+    
+    # Validate file
+    if (is.null(disease_list$EpiTrax_name) || is.null(disease_list$Public_name)) {
+      stop("File '", fpath, "' is incorrectly formatted. Please use the column 
+           names: 'EpiTrax_name' and 'Public_name'.")
+    }
+    
+    disease_list
+  }
 }
 
 # ------------------------------------------------------------------------------
 # Read in EpiTrax data ---------------------------------------------------------
 epitrax_data <- read_epitrax_data()
-
-# Format EpiTrax Data
-# - Uncomment to format with onset_date
-# epitrax_data <- format_onset_date(epitrax_data)
-# - Format with patient_mmwr_week
-epitrax_data <- format_week_num(epitrax_data)
 
 # Rearrange columns (for DEBUGGING)
 epitrax_data <- epitrax_data[,c("disease", "month", "year", "counts")]
@@ -194,9 +245,9 @@ avgs_fname <- with(epitrax_data_5yr,
                    paste0("monthly_avgs_", min(year), "-", max(year), ".csv"))
 write_internal_report(monthly_5yr_avgs, avgs_fname)
 
-# Prepare Public Report
-public_disease_list <- read.csv("other_data/public_report_disease_names.csv", 
-                                header = TRUE)
+# ------------------------------------------------------------------------------
+# Prepare Public Report --------------------------------------------------------
+public_disease_list <- get_public_disease_list(epitrax_data)
 
 # - Remove rows from monthly_5yr_avgs that aren't going into the public report
 monthly_5yr_avgs <- subset(monthly_5yr_avgs, 
@@ -205,18 +256,21 @@ monthly_5yr_avgs <- subset(monthly_5yr_avgs,
 missing_diseases <- subset(public_disease_list, 
                            !(EpiTrax_name %in% monthly_5yr_avgs$disease))
 
-# - Fill the missing diseases in with avg = 0
-missing_5yr_avgs <- data.frame(
-  disease = missing_diseases$EpiTrax_name
-)
-present_colnames <- colnames(monthly_5yr_avgs)
-missing_colnames <- present_colnames[2:length(present_colnames)]
-missing_5yr_avgs[, missing_colnames] <- 0.0
-
-# - Combine monthly_5yr_avgs with missing_5yr_avgs
-monthly_5yr_avgs <- rbind(monthly_5yr_avgs, missing_5yr_avgs)
-monthly_5yr_avgs <- monthly_5yr_avgs[order(monthly_5yr_avgs$disease),]
-
+# If there are any missing diseases, add them
+if (length(missing_diseases$EpiTrax_name) > 0) {
+  # - Fill the missing diseases in with avg = 0
+  missing_5yr_avgs <- data.frame(
+    disease = missing_diseases$EpiTrax_name
+  )
+  present_colnames <- colnames(monthly_5yr_avgs)
+  missing_colnames <- present_colnames[2:length(present_colnames)]
+  missing_5yr_avgs[, missing_colnames] <- 0.0
+  
+  # - Combine monthly_5yr_avgs with missing_5yr_avgs
+  monthly_5yr_avgs <- rbind(monthly_5yr_avgs, missing_5yr_avgs)
+  # - Sort alphabetically so missing diseases are correctly placed
+  monthly_5yr_avgs <- monthly_5yr_avgs[order(monthly_5yr_avgs$disease),]
+}
 
 # - Most recent month is the maximum month included in the data minus 1
 current_report_year <- max(monthly_counts$year)
